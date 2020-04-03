76ee068aeef23e67fa955f723f5826e83c0351b6
Romain <cassignol@secib.fr>
2020-04-03 11:43:14 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const port = 1321;
+const ctLengthHeader = "content-length";
+const CRLF = "<br />"
+
+let body = "";
+
+function readBodyAsString(req, callback) {
+    let body = "";
+    let eof = +req.headers[ctLengthHeader];
+    
+    req.on("data", (chunk) => {        
+        body += chunk.toString();        
+        if(eof === body.length) {
+            callback(body);
+        }
+    }); 
+}
+
+app.get("*", (req, res) => {
+    res.send(`GET:${req.path}`);
+});
+
+app.post("*", (req, res) => {
+    readBodyAsString(req, (body) => {
+        res.send(`POST:${req.path}${CRLF}BODY:${body}`);      
+    });    
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is  now listening on port ${port}`);
+});
+
